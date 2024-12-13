6d9ffce315611780642ff76c9674539053873ea5
GeorgeGinger <retez600@gmail.com>
2024-12-13 19:58:09 +0100

    save data and generate html for delivery optioon

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,14 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [];
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
 
-// [{
-// 	productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-// 	quantity: 2
-// },{
-// 	productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-// 	quantity: 10
-// }];
+[{
+	productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+	quantity: 2,
+	deliveryOptionId: '2'
+},{
+	productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+	quantity: 10,
+	deliveryOptionId: '1'
+}];
 
 export function saveToStorage() {
 	localStorage.setItem('cart', JSON.stringify(cart));
@@ -57,7 +59,8 @@ export function editQuantityItem(productId, quantity) {
 	 }else {
 		cart.push({
 		  productId,
-		  quantity
+		  quantity,
+		  deliveryOptionId: 1
 		});
 	 }
 
@@ -87,3 +90,21 @@ export function editQuantityItem(productId, quantity) {
 
 		return cartQuantity;
 	}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+
+	//  vyulovani matchingItem pri kliknuti na Add to cart
+	let matchingItem;
+	
+	// prohledani kosiku zdali v nem pridavany produkt uz je
+	cart.forEach((cartItem) => {
+	if(productId === cartItem.productId) {
+		// ulozime refelenci item do matchingItem
+		matchingItem = cartItem;
+	}
+	});
+
+	matchingItem.deliveryOptionId = deliveryOptionId;
+
+	saveToStorage();
+}
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,6 @@
 export function formatCurrency(priceCents) {
 	return (priceCents / 100).toFixed(2);
 }
+
+// umoznuje exportovat pouze jednu vec
+export default formatCurrency;
