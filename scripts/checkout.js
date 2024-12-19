e70b051a3ec45e80d2850df7e28124de3c88c7cd
GeorgeGinger <retez600@gmail.com>
2024-12-19 17:03:29 +0100

    checkout pomoci ajaxu, pouziti arrow fce jako parametru fce

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,13 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import {loadProducts} from '../data/products.js';
 // jina sintaxe spusti cod v zadanem souboru
 // import '../data/cart-oop.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+	renderOrderSummary();
+	renderPaymentSummary();
+});
+
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -7,8 +7,6 @@ import {renderPaymentSummary} from './paymentSummary.js'
 // default export esm library
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
-
-
 export function renderOrderSummary() {
 
   let cartSummaryHTML = "";
